291112b89d6862c0fe295b3b21407ef68fb48c73
Yaroslav <116947585+poprotsckyi@users.noreply.github.com>
2023-08-30 16:35:00 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -88,3 +88,4 @@ module.exports = {
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
     'no-dupe-class-members': 'off',
     'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'no-undef': 'off',
