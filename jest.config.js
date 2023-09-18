523f5da2b2d7bc1f0bc7b791b56a773a219ab7ea
Yaroslav <116947585+poprotsckyi@users.noreply.github.com>
2023-09-18 16:43:39 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -12,3 +12,6 @@ module.exports = {
   modulePaths: ['src'],
 
   // This fixed an error related to the CSS and loading gif breaking
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|scss|css)$':
+      '<rootDir>/src/core/__mocks__/fileMock.ts',
