669dcf6b7453ac78fd9ab7c778dfd62296fd27e9
Yaroslav <116947585+poprotsckyi@users.noreply.github.com>
2023-09-19 14:59:04 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -15,3 +15,5 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|scss|css)$':
       '<rootDir>/src/core/__mocks__/fileMock.ts',
+  },
+  coverageDirectory: '<rootDir>/coverage',
